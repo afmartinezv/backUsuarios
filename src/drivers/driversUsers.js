c4aab72134d94f880ff9e2e938b35c2814de7e66
afmartinezv <andresmartinezvaron@gmail.com>
2024-01-08 18:36:53 -0500

    Actualizaciones

--- src/drivers/driversUsers.js
+++ src/drivers/driversUsers.js
@@ -12,22 +12,38 @@ createUser: async (request,response) => {
                 datos: userCreate._id});
         }
     } catch (error) {
-        response.json({message: 'Ocuriro un error'});
+        response.json({message: 'Ocuriro un error'
+    });
     }
 },
 viewUsers:async (request,response) => {
     try {
         const fullUsers = await ModelsUsers.find();
-        response.json({message: 'Todos los usuarios', fullUsers});
+        const users = [];
+        for (let i=0; i < fullUsers.length; i++){
+            const objeto ={
+                id: fullUsers[i]._id,
+                username: fullUsers[i].username,
+            };
+            users.push(objeto);
+        }
+        response.json({
+            result: 'bien',
+            message: 'todos los regalos', 
+            datos: users});
     } catch (error) {
-        response.json({message: 'Ocurrio un error'});
+        response.json({
+            result: 'mal',
+            message: 'Ocurrio un error',
+            datos: error
+        });
     }
 },
 
 viewUser:async (request,response) => {
     try {
         const users = await ModelsUsers.findById(request.params.id);
-        response.json({message: 'Todos los usuarios', users});
+        response.json({message: 'Usuario Leido', users});
     } catch (error) {
         response.json({message: 'Ocurrio un error'});
     }
@@ -53,10 +69,20 @@ updateUser: async (request, response) => {
     },
 deleteUser:async (request,response) => {
     try {
-        const del = await ModelsUsers.findByIdAndDelete(request.params.id);
-        response.json({message: 'Todos los usuarios', del});
+        const del = await ModelsUsers.findByIdAndDelete(
+            request.params.id
+        );
+        response.json({
+            result: 'bien',
+            message: 'Todos los Usuario eliminado', 
+            datos: del._id
+        });
     } catch (error) {
-        response.json({message: 'Ocurrio un error'});
+        response.json({
+            result: 'mal',
+            message: 'Ocurrio un error',
+            datos: error,
+        });
     }
 },
 };
