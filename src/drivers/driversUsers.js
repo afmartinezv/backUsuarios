04d5d0eedd5b1027452de7af5583d78b0f8aaa63
afmartinezv <andresmartinezvaron@gmail.com>
2024-01-08 15:11:57 -0500

    Actualizaciones

--- src/drivers/driversUsers.js
+++ src/drivers/driversUsers.js
@@ -6,7 +6,10 @@ createUser: async (request,response) => {
         const newUser = new ModelsUsers(request.body);
         const userCreate = await newUser.save();
         if(userCreate._id){
-            response.json({message: 'Usuario creado'});
+            response.json({
+                resultado: 'bien',
+                mensaje: 'Usuario creado',
+                datos: userCreate._id});
         }
     } catch (error) {
         response.json({message: 'Ocuriro un error'});
@@ -29,9 +32,25 @@ viewUser:async (request,response) => {
         response.json({message: 'Ocurrio un error'});
     }
 },
-updateUser:(request,response) => {
-    response.json({message: 'Hola desde PUT regalos'});
-},
+updateUser: async (request, response) => {
+    try {
+      const actualizado = await ModelsUsers.findByIdAndUpdate(
+        request.params.id,
+        request.body
+      );
+      response.json({
+        resultado: 'bien',
+        mensaje: 'regalo actualizado',
+        datos: actualizado._id,
+      });
+    } catch (error) {
+        response.json({
+          resultado: 'mal',
+          mensaje: 'ocurrió un error',
+          datos: error,
+        });
+      }
+    },
 deleteUser:async (request,response) => {
     try {
         const del = await ModelsUsers.findByIdAndDelete(request.params.id);
